4c7e751a466d1fda96c0185bc8f0c4a795c3362d
rareid <rareid@mtu.edu>
2017-01-15 01:22:50 -0500

    Removing Unwanted Comments

--- src/MainGui.java
+++ src/MainGui.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
 
+@SuppressWarnings("serial")
 public class MainGui extends Frame implements ActionListener, WindowListener{
 	private Label lblCount;    // Declare a Label component 
 	private TextField tfCount; // Declare a TextField component 
@@ -11,42 +12,27 @@ public class MainGui extends Frame implements ActionListener, WindowListener{
 	
 	public MainGui(){
 	      setLayout(new FlowLayout());
-	         // "super" Frame (a Container) sets its layout to FlowLayout, which arranges
-	         // the components from left-to-right, and flow to next row from top-to-bottom.
+	      
+	      lblCount = new Label("Enter Number Of Weapons Desired (-1 for random)");
+	      add(lblCount);                   
 	 
-	      lblCount = new Label("Enter Number Of Weapons Desired (-1 for random)");  // construct the Label component
-	      add(lblCount);                    // "super" Frame adds Label
+	      tfCount = new TextField("0", 10); 
+	      tfCount.setEditable(true);      
+	      add(tfCount);                     
 	 
-	      tfCount = new TextField("0", 10); // construct the TextField component
-	      tfCount.setEditable(true);       // set to read-only
-	      add(tfCount);                     // "super" Frame adds TextField
-	 
-	      btnCount = new Button("Generate Weapons");   // construct the Button component
-	      add(btnCount);                    // "super" Frame adds Button
+	      btnCount = new Button("Generate Weapons");  
+	      add(btnCount);                    
 	      
 	      displayWeapons = new TextArea(20,40);
 	      add(displayWeapons);
 	      btnCount.addActionListener(this);
-	         // btnCount is the source object that fires ActionEvent when clicked.
-	         // The source add "this" instance as an ActionEvent listener, which provides
-	         //  an ActionEvent handler called actionPerformed().
-	         // Clicking btnCount invokes actionPerformed().
+
 	 
-	      setTitle("GM Helper");  // "super" Frame sets its title
-	      setSize(1200, 600);        // "super" Frame sets its initial window size
-	 
-	      // For inspecting the components/container objects
-	      // System.out.println(this);
-	      // System.out.println(lblCount);
-	      // System.out.println(tfCount);
-	      // System.out.println(btnCount);
+	      setTitle("GM Helper");  
+	      setSize(1200, 600);       
+	      
 	      addWindowListener(this);
-	      setVisible(true);         // "super" Frame shows
-	 
-	      // System.out.println(this);
-	      // System.out.println(lblCount);
-	      // System.out.println(tfCount);
-	      // System.out.println(btnCount);
+	      setVisible(true);   
 	}
 
 	public static void main(String[] args) {
