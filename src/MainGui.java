088623f41d53a73bc47cb08d4c727dc771a10feb
rareid <rareid@mtu.edu>
2017-01-14 22:27:56 -0500

    started to create gui

---
+++ src/MainGui.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.awt.*;
+
+public class MainGui extends Frame{
+	
+	public MainGui(){
+		
+	}
+
+	public static void main(String[] args) {
+		new MainGui();
+	}
+	
+	
+}
